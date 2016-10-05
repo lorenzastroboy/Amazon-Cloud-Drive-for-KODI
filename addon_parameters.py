ceb3bec1d710bbd2d09dff6a78b422b629e5b133
Dylan Durdle <ddurdle@gmail.com>
2016-10-04 20:46:50 -0400

    new files

---
+++ addon_parameters.py
@@ -0,0 +1,34 @@
+'''
+    gdrive (Google Drive ) for KODI / XBMC Plugin
+    Copyright (C) 2013-2016 ddurdle
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+'''
+
+# cloudservice - standard XBMC modules
+import xbmc, xbmcgui, xbmcplugin, xbmcaddon, xbmcvfs
+
+#from resources.lib import gdrive
+from resources.lib import acd_api
+
+# global variables
+PLUGIN_NAME = 'acd'
+#addon = xbmcaddon.Addon(id='plugin.video.gdrive')
+addon = xbmcaddon.Addon(id='plugin.video.gdrive-testing')
+
+cloudservice2 = acd_api.acd
+cloudservice1 = acd_api.acd
+
