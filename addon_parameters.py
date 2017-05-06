e6a08ad6ff93ac30c861015c324a0adaacc81e3c
Dylan Durdle <ddurdle@gmail.com>
2017-05-06 09:17:41 -0400

    promote to stable

--- addon_parameters.py
+++ addon_parameters.py
@@ -26,7 +26,8 @@ from resources.lib import acd_api
 
 # global variables
 PLUGIN_NAME = 'acd'
-addon = xbmcaddon.Addon(id='plugin.video.acd-testing')
+#addon = xbmcaddon.Addon(id='plugin.video.acd-testing')
+addon = xbmcaddon.Addon(id='plugin.video.acd')
 
 cloudservice2 = acd_api.acd
 cloudservice1 = acd_api.acd
